43b512fd38591eed23d6ea342a38bc88fdfbc636
ceragos <antonio.casta_yo@hotmail.com>
2021-01-24 22:41:42 -0500

    Configuración de firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCvB7M2yBoYDe8Oj3S1K52lOpYFiwVwS84",
+    authDomain: "j-chat-app.firebaseapp.com",
+    projectId: "j-chat-app",
+    storageBucket: "j-chat-app.appspot.com",
+    messagingSenderId: "1026232876231",
+    appId: "1:1026232876231:web:f3c13e330afda81ab1d2e2",
+    measurementId: "G-4V4KHPT3GG"
+}
